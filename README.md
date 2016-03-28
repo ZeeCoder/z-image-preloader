e8dfe82151c63fc2c6251dea92e67d2b58619b54
Viktor Hubert <viktor.hubert@eventstag.com>
2016-03-28 11:49:56 +0100

    Refactored the code to address some issues.
    - Added bluebird as a Promises/A+ implementation instad of Q.
    - Replaced the Image constructor with the document.createElement call.
    - Removed jQuery related functionality.
    
    Resolves #1, resolves #2.
    
    This marks the release of 1.0.0.

--- README.md
+++ README.md
@@ -1,44 +1,31 @@
 # z-image-preloader
 [![npm version](https://badge.fury.io/js/z-image-preloader.svg)](http://badge.fury.io/js/z-image-preloader)
 
-Creates a [promise object](https://promisesaplus.com/) which is resolved when all the relevant images are
-loaded. [Kriskowal's Q](https://github.com/kriskowal/q) is used as a promise implementation.
+Creates a [promise object](https://promisesaplus.com/) which is resolved when
+all images are loaded. [bluebird](bluebirdjs.com) is used as a Promises/A+ implementation.
 
-Since it's a CommonJS module, it must be used alongside with [Browserify](http://browserify.org/), or
-something similar, like [WebPacker](http://webpack.github.io/).
+Since it's a CommonJS module, you must use a build tool to use it, like [Browserify](http://browserify.org/), or [Webpack](http://webpack.github.io/).
 
 ## Example, explanation
 
-```html
-<div id="an-element-containing-images">
-    <img src="the-src">
-    <img src="the-src">
-    <img src="the-src">
-    <div>
-        <img src="the-src">
-    </div>
-</div>
-```
-
 ```js
-var image_preloader = require('z-image-preloader');
+var preloader = require('z-image-preloader');
 
-// The `getPromise` method accepts url strings, an array of strings, a jQuery
-// object, or an array containing strings and / or jQuery objects.
-var promise = image_preloader.getPromise([
-    'some url',
-    'an other url',
-    $('#an-element-containing-images')
+// The `load` method accepts an array of url strings. (Or a single url.)
+var promise = preloader.load([
+    'http://some-url/some-image.png',
+    'http://some-other-url/some-other-image.jpg',
+    'non-existent-image'
 ]);
 
-// The returned promise will be resolved, when all images are loaded
+// The returned promise will be resolved, when all images are loaded (or failed to load)
 promise.then(function() {
-    // Do something when preloading is done
+    // Do something when preloading is done.
+    // The arguments here will contain the loaded image objects created by `document.createElement('img')` and possible error `Event` objects.
+    // The above call for example will return with two `HTMLImageElement` and an `Event` object.
 });
+//.catch(function(){}) // No reason to do this, the promise will always resolve. I promise. ;)
 ```
-
-## Testing
-Tests are in a work-in-progress state.
 
 ## License
 [MIT](LICENSE)
--- image-preloader.js
+++ image-preloader.js
@@ -1,75 +1,67 @@
 'use strict';
-var $ = require('jquery');
-var q = require('q');
+
+var Promise = require('bluebird');
 
 module.exports = {
     /**
-     * string|jQuery|Array data Url string or strings, or a jQuery object
-     * containing `img` elements.
+     * @param {String|Array} data Url string or array of strings.
+     * @returns {Promise}
      */
-    getPromise: function(data) {
-        var self = this;
-
-        // Converting a single element to an array for consistency.
+    load: function(data) {
+        // Converting a single element to an array.
         if (Array.isArray(data) === false) {
             data = [data];
         }
 
-        var imgPromises = [];
+        // Promisify!
+        data = data.map(function(str) {
+            // Maybe there's a better solution to implement a "settle" functionality for es6 promises?
+            return new Promise(function(resolve) {
+                this.getPromise(str)
+                    .then(function(param) { resolve(param); })
+                    .catch(function(param) { resolve(param); });
+            }.bind(this));
+        }.bind(this));
 
-        var dataLength = data.length;
-        var $images = null;
-        var img = null;
-        for (var i = dataLength - 1; i >= 0; i--) {
-            if (data[i] instanceof $) {
-                if (data[i].is('img')) {
-                    imgPromises.push(this.createPromiseForImg(data[i][0]));
-                } else {
-                    $images = data[i].find('img');
-                    $images.each(function() {
-                        imgPromises.push(self.createPromiseForImg(this));
-                    });
-                }
-            } else if (typeof data[i] === 'string') {
-                imgPromises.push(self.createPromiseForImg(data[i]));
-            }
-        }
-
-        return q.all(imgPromises);
+        return Promise.all(data);
     },
 
     /**
-     * string|DOMElement img Can be a string representing the image url, or an
-     * Image DOM element.
+     * @param {string|HTMLImageElement} img Can be a string representing the
+     * image url, or an Image DOM element.
+     * @returns {Promise}
      */
-    createPromiseForImg: function(img) {
-        if (typeof img === 'string') {
-            var imgSrc = img;
-            img = new Image();
-        }
+    getPromise: function(img) {
+        return new Promise(function(resolve, reject) {
+            if (
+                img instanceof HTMLImageElement === false &&
+                typeof img !== 'string'
+            ) {
+                console.warn('The provided `img` was not a string or an HTMLImageElement object. Using an empty string instead.');
+                img = '';
+            }
 
-        var d = q.defer();
-        img.onload = function () {
-            d.resolve(img);
-        };
+            if (typeof img === 'string') {
+                var imgSrc = img;
+                img = document.createElement('img');
+            }
 
-        img.onabort = function (e) {
-            d.reject(e);
-        };
+            img.onload = function() {
+                resolve(img);
+            };
 
-        img.onerror = function (e) {
-            d.reject(e);
-        };
+            img.onabort = img.onerror = function(err) {
+                reject(err);
+            };
 
-        if (typeof imgSrc !== 'undefined') {
-            img.src = imgSrc;
-        }
+            if (typeof imgSrc !== 'undefined') {
+                img.src = imgSrc;
+            }
 
-        // Cached images immediately resolve their promises.
-        if (img.complete) {
-            d.resolve(img);
-        }
-
-        return d.promise;
+            // Cached images immediately resolve their promises.
+            if (img.complete) {
+                resolve(img);
+            }
+        });
     }
 };
